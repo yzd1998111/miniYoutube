6983268c1767c8c67a5cb2dd51a25fa13a926a71
yzd1998111 <1052703078@qq.com>
2018-06-14 14:46:01 -0400

    Get the Youtube API working and now the app can play Youtube videos

---
+++ app/src/main/java/com/example/yuanzhendong/miniyoutube/YoutubeActivity.java
@@ -0,0 +1,13 @@
+package com.example.yuanzhendong.miniyoutube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YoutubeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube);
+    }
+}
